de72261335d36a7399427fbeb522c332f1400d7a
leo <leo.lemaire007@orange.fr>
2024-08-14 11:34:25 +0200

    batch

--- convert.py
+++ convert.py
@@ -1,14 +1,12 @@
-# Takes a filename as input, converts it from prometheus to grafana
-# Return the output to the second input
-
 import json
 import sys
+import os
 
 def convert_target(target):
     n_target = {}
     n_target["datasource"] = {
         "type": "influxdb",
-        "uid": "influxdbsource",
+        "uid": "{{ influxdb_datasource_uid }}",
     }
 
     field = target["expr"].split("{")[0]
@@ -16,7 +14,6 @@ def convert_target(target):
     n_target["query"] = f"""
         from(bucket: \"DHBW\")
             |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
-            |> filter(fn: (r) => r[\"_measurement\"] == "electrolyzer\")
             |> filter(fn: (r) => r[\"_field\"] == \"{field}\")
             |> aggregateWindow(every: v.windowPeriod, fn: last, createEmpty: false)
             |> yield(name: \"last\")
@@ -50,8 +47,10 @@ def convert(dash):
     n_dash["panels"] = convert_panels(dash["dashboard"]["panels"].copy())
     return n_dash
 
-with open(sys.argv[1]) as f:
-    dash = json.load(f)
-    res = convert(dash)
-    with open(sys.argv[2], "w") as out:
-        json.dump(res, out)
+for file in os.listdir("prometheus"):
+    print(f"converting {file}")
+    with open("prometheus/"+file) as f:
+        dash = json.load(f)
+        res = convert(dash)
+        with open("influx/"+file, "w") as out:
+            json.dump(res, out)
--- download_all.py
+++ download_all.py
@@ -7,6 +7,6 @@ dashs = {d["uri"].split("/")[1]: d["uid"] for d in resp}
 
 for name in dashs:
     print("Downloading "+name)
-    with open(f"{name}.json", "w") as out:
+    with open(f"prometheus/{name}.json", "w") as out:
         resp = requests.get("http://141.72.13.23:3000/api/dashboards/uid/"+dashs[name]).json();
         json.dump(resp, out, indent=4)
