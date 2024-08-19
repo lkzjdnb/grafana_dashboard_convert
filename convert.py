189db4b25199d0fbfaa63126c8ffe1e3bfe38c69
leo <leo.lemaire007@orange.fr>
2024-08-19 11:10:46 +0200

    Convert transformations names

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import os
+import yaml
 
 def convert_target(target):
     n_target = {}
@@ -27,6 +28,41 @@ def convert_targets(targets):
 
     return targets
 
+def get_dict_from_string(inp):
+    out = {}
+    for f in inp.strip("{}").split(","):
+        name, value = f.split("=")
+        value = value.strip("\"")
+        out[name] = value
+    return out
+
+def convert_trans_fields(fields):
+    n_fields = {}
+    for f in fields:
+        if f.startswith("{__name__"):
+            rec = get_dict_from_string(f)
+            n_fields[rec["__name__"]] = fields[f]
+        else:
+            n_fields[f] = fields[f]
+    return n_fields
+    
+def convert_trans_options(opt):
+    opt["fields"] = convert_trans_fields(opt["fields"].copy())
+    return opt
+    
+def convert_transformation(trans):
+    if trans["id"] != "groupBy":
+        return trans
+    trans["options"] = convert_trans_options(trans["options"].copy())
+
+    return trans
+
+def convert_transformations(transformations):
+    for i, trans in enumerate(transformations):
+        transformations[i] = convert_transformation(trans.copy())
+
+    return transformations
+
 def convert_panel(panel):
     panel["datasource"] = {
         "type": "influxdb",
@@ -34,6 +70,8 @@ def convert_panel(panel):
     }
 
     panel["targets"] = convert_targets(panel["targets"].copy())
+    if "transformations" in panel:
+        panel["transformations"] = convert_transformations(panel["transformations"].copy())
 
     return panel
 
